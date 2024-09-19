75dec6a6b3b937ec7ec752031525e3fdabc4ac61
BRYOOH <bryoohmuchira@gmail.com>
2024-09-19 19:46:44 +0300

    renderlink

--- src/components/Addproduct/Addproduct.jsx
+++ src/components/Addproduct/Addproduct.jsx
@@ -30,7 +30,7 @@ const Addproduct = () => {
     let formData = new FormData();
     formData.append('product', image);
 
-    await fetch('http://localhost:4000/upload',{
+    await fetch('https://onlineshop-backend-63tt.onrender.com/upload',{
       method: 'POST',
       Headers:{
         Accept: 'application/json',
@@ -41,7 +41,7 @@ const Addproduct = () => {
       product.image = responseData.image_url;
       console.log(product);
 
-      await fetch('http://localhost:4000/addproduct',{
+      await fetch('https://onlineshop-backend-63tt.onrender.com/addproduct',{
          method: 'POST',
          headers:{
           Accept: 'application/json',
